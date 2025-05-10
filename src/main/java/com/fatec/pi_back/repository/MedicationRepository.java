e5a6eb32ca5b3a5ec279094a7b49a1cd014cc84f
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2025-05-10 20:20:17 -0300

    :sparkles: feat: Medication Repository

---
+++ src/main/java/com/fatec/pi_back/repository/MedicationRepository.java
@@ -0,0 +1,8 @@
+package com.fatec.pi_back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.fatec.pi_back.domain.Medication.Medication;
+
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+    
+}
