b651adfcef46949b96d79c3a41224f9865a49352
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2025-05-10 13:07:18 -0300

    :bug: fix: fixed error while testing initialize project

--- src/test/java/com/fatec/pi_back/PiBackApplicationTests.java
+++ src/test/java/com/fatec/pi_back/PiBackApplicationTests.java
@@ -3,7 +3,7 @@ package com.fatec.pi_back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestConfig.class)
 class PiBackApplicationTests {
 
 	@Test
