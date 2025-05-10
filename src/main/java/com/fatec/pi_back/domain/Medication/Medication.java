ff8bcdca749e0d981bc85cb26ec0d05239fcc02e
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2025-05-10 20:19:05 -0300

    :sparkles: feat: Medication Domain

---
+++ src/main/java/com/fatec/pi_back/domain/Medication/Medication.java
@@ -0,0 +1,56 @@
+package com.fatec.pi_back.domain.Medication;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fatec.pi_back.domain.User.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "medication")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Medication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 255)
+    private String medication_function;
+
+    @Column(name = "dosage_weight")
+    private Float dosageWeight;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", updatable = false)
+    private User createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "updated_by")
+    private User updatedBy;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+    public Medication(String name, String medication_function, Float dosageWeight) {
+        this.name = name;
+        this.medication_function = medication_function;
+        this.dosageWeight = dosageWeight;
+    }
+}
---
+++ src/main/java/com/fatec/pi_back/domain/Medication/MedicationDTO.java
@@ -0,0 +1,5 @@
+package com.fatec.pi_back.domain.Medication;
+
+public record MedicationDTO (String name, String medication_function, Float dosageWeight){
+    
+}
